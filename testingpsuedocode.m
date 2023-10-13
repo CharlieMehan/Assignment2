afb556165de8e246f9a6064439f58ee20c092834
CharlieMehan <61522532+CharlieMehan@users.noreply.github.com>
2023-10-13 19:53:32 +1100

    Add files via upload

---
+++ testingpsuedocode.m
@@ -0,0 +1,4 @@
+%Robot will pickup bottle and move arm around to where glasses are situated
+%Robot will pour a drink according to parameters set out based on external
+%input value for selected drink and quantity of drink
+%robot will pour and then return bottle to original position
